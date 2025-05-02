6a8c18d33ec9598d9e5217ee055c8091654ffd53
Андрей <andreybelo73.30@gmail.com>
2025-05-02 19:22:32 +0300

    second

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # oip-demo
 
-## Запуск
+## Запуск в докере
 ```bash
 docker build -t vector-search .
 docker run -p 8000:8000 vector-search
+```
+
+## Запуск локально
+```bash
+py main.py
 ```
--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -30,3 +33,7 @@ def search(request: Request, query: str = Form(...)):
     results = index.search(query, 10)
     matches = [(documents[r[0]], r[1]) for r in results]
     return templates.TemplateResponse("index.html", {"request": request, "results": matches, "query": query})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
